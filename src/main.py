32de7ecdb11ba379f8a691f7a3838bb7fe14181c
Chris Rios <robotman40@protonmail.com>
2026-01-31 05:00:41 +0000

    Remove container functionality for the time being

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from containers import router as containers
 
 app = FastAPI()
 
-app.include_router(containers, prefix="/containers")
+# app.include_router(containers, prefix="/containers")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)

01e539ba3f582109eaf816a32e82008ba9d922d0
Chris Rios <31458662+robotman40@users.noreply.github.com>
2026-02-06 22:45:01 -0800

    Remove hardcoded SECRET_KEY from security.py
    Remove hardcoded secret key for security reasons.

--- src/security/security.py
+++ src/security/security.py
@@ -6,7 +6,7 @@ import fastapi.security as security
 from jwt import InvalidTokenError
 from pwdlib import PasswordHash
 
-SECRET_KEY = "46810de1ffee764917b80678b169d1b829ceda1b2ddd0a8e01b88cb01902dbf9"
+SECRET_KEY = ""
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 

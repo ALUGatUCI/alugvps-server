5561e767b3e0d154501af7b85c4f8e417c1622b0
Chris Rios <robotman40@protonmail.com>
2026-03-15 23:34:37 +0000

    Implement ban handling for admin CLI tool

--- src/serveradmin.py
+++ src/serveradmin.py
@@ -3,7 +3,7 @@
 import pathlib
 import sqlmodel
 import sys
-from database.models import Request
+from database.models import Request, Account
 from database.containers import create_new_container
 
 database_path = pathlib.Path.home() / ".alugvps-server" / "alugvps.db"
@@ -14,11 +14,6 @@ session = sqlmodel.Session(engine)
 arguments = sys.argv
 
 if __name__ == "__main__":
-    if len(arguments) == 2:
-        if arguments[1] == "list":
-            requests = session.exec(sqlmodel.select(Request)).all()
-            for request in requests:
-                print(f"ID: {request.id}\nBody: {request.request}\n\n")
     if len(arguments) == 3:
         if arguments[1] == "delete":
             request = session.get(Request, int(arguments[2]))
@@ -49,3 +44,32 @@ if __name__ == "__main__":
                 session.commit()
             else:
                 print(f"No request found with ID {arguments[2]}")
+        if arguments[1] == "list":
+            if arguments[2] == "requests":
+                requests = session.exec(sqlmodel.select(Request)).all()
+                for request in requests:
+                    print(f"ID: {request.id}\nBody: {request.request}\n\n")
+            elif arguments[2] == "users":
+                users = session.exec(sqlmodel.select(Account)).all()
+                for user in users:
+                    print(f"ID: {user.id}\nEmail: {user.email}\nBanned: {user.banned}\nConfirmed: {user.confirmed}\n\n")
+            else:
+                print(f"Unknown list type: {arguments[2]}")
+        if arguments[1] == "ban":
+            user = session.get(Account, int(arguments[2]))
+            if user is not None:
+                user.banned = True
+                session.commit()
+                print(f"Banned user with ID {arguments[2]}")
+            else:
+                print(f"No user found with ID {arguments[2]}")
+        if arguments[1] == "unban":
+            user = session.get(Account, int(arguments[2]))
+            if user is not None:
+                user.banned = False
+                session.commit()
+                print(f"Unbanned user with ID {arguments[2]}")
+            else:
+                print(f"No user found with ID {arguments[2]}")
+    else:
+        print("Implement help later")

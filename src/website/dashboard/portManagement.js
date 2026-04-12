5bde41b0e697a306c422db2f431b202e35661d84
Chris Rios <robotman40@protonmail.com>
2026-04-12 23:16:09 +0000

    Support deleting existing ports

--- src/website/dashboard/portManagement.js
+++ src/website/dashboard/portManagement.js
@@ -1,24 +1,51 @@
 export async function savePort(portName, listen, connect) {
-    const params = new URLSearchParams({'name': portName, 'listen': listen, 'connect': connect});
+    const params = new URLSearchParams({
+        name: portName,
+        listen: listen,
+        connect: connect,
+    });
     return fetch(`/containers/port/add?${params}`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
         },
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            if (data.success) {
+                alert("The port has successfully be submitted");
+            } else {
+                alert(`An issue occurred submitting the port: ${data.message}`);
+            }
+        })
+        .catch((error) => {
+            alert(`An error occurred trying to submit the port: ${error}`);
+        });
+}
 
+export async function deletePort(portName) {
+    const params = new URLSearchParams({ name: portName });
+    return fetch(`/containers/port/delete?${params}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
     })
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        if (data.success) {
-            alert("The port has successfully be submitted");
-        } else {
-            alert(`An issue occurred submitting the port: ${data.message}`)
-        }
-    })
-    .catch(error => {
-        alert(`An error occurred trying to submit the port: ${error}`)
-    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            if (data.success) {
+                alert("The port has successfully be deleted");
+            } else {
+                alert(`An issue occurred deleting the port: ${data.message}`);
+            }
+        })
+        .catch((error) => {
+            alert(`An error occurred trying to delete the port: ${error}`);
+        });
 }

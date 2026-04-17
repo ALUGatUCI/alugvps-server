3e15744c4bcbed4ad8f9fead28bdb511e8f56c23
Chris Rios <robotman40@protonmail.com>
2026-04-17 23:00:24 +0000

    Add events.py

--- src/communications/events.py
+++ src/communications/events.py
@@ -2,24 +2,24 @@ import communications.communications as communications
 from database.models import Account
 
 async def _get_ucinetid_email(user_id: int, sql_session: session) -> str:
-    user = await sql_session.get(Account.email, user_id)
-    return user.ucinetid
+    email = await sql_session.get(Account.email, user_id)
+    return user
 
 async def approval_email(user_id: int, sql_session: session):
     email = await _get_ucinetid_email(user_id, sql_session)
     await communications.send_email(
         "Container Approval",
-        f"{email}@uci.edu",
+        email,
         (f"Hello there,\n"
         f"Your container has been approved. Thank you for using ALUG@UCI Community VPS Services.\n"
-        f"Here is your temporary login password: {email}")
+        f"Here is your temporary login password: {sql_session.get(Account.confirmation_code, user_id)}")
     )
 
 async def not_selected_email(user_id: int, sql_session: session):
     email = await _get_ucinetid_email(user_id, sql_session)
     await communications.send_email(
         "Container Not Selected",
-        f"{email}@uci.edu",
+        email,
         ("Hello there,\n"
         "Thank you for applying for a container. After a careful review of your request, we regret to inform you that your container has not been selected."
         "We have limited server capacity, which makes us unable to fulfill your request at this time.\n"
